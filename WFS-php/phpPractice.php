98d4f7e44ab7f57116ccc861d8d14d83773ee9ad
Sanju2op <120462833+Sanju2op@users.noreply.github.com>
2023-07-09 21:37:10 +0530

    added WFS-php and organized files

---
+++ WFS-php/phpPractice.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>
+      My First PHP Hello World!
+    </title>
+  </head>
+  <body>
+<?php
+echo "Hello World!";
+$a = 10;
+$b = 20;
+$a + $b;
+echo "Sum is $a";
+    ?>
+  </body>
+</html>
+
+
+
+
